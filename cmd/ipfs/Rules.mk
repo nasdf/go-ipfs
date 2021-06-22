b03f1ce0f8cd3911077b6133a2415875d309d876
Keenan Nemetz <keenan.nemetz@gmail.com>
2021-06-22 14:42:28 -0700

    force static compile

--- cmd/ipfs/Rules.mk
+++ cmd/ipfs/Rules.mk
@@ -13,7 +13,7 @@ PATH := $(realpath $(d)):$(PATH)
 # DEPS_OO_$(d) += merkledag/pb/merkledag.pb.go namesys/pb/namesys.pb.go
 # DEPS_OO_$(d) += pin/internal/pb/header.pb.go unixfs/pb/unixfs.pb.go
 
-$(d)_flags =-ldflags="-buildid= -s -w -X "github.com/ipfs/go-ipfs".CurrentCommit=$(git-hash)"
+$(d)_flags =-ldflags="-s -w -X "github.com/ipfs/go-ipfs".CurrentCommit=$(git-hash) -buildid= -extldflags=-static"
 
 $(d)-try-build $(IPFS_BIN_$(d)): GOFLAGS += $(cmd/ipfs_flags)
 
